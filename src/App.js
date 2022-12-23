16f86a600beed8b7adc86a50d898a0196f6bcb22
anes <ani02mas@gmail.com>
2022-12-23 20:44:34 +0100

    react js

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import  Container  from 'react-bootstrap/Container';
+ import  Form  from 'react-bootstrap/From';
+function App() {
+  return (
+    <Container>
+      <Form>
+        <img src='logo.jpg'></img>
+      
+      </Form>
+    </Container>
+  );
+}
+
+export default App;
